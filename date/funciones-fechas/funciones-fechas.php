a70c3a20aad62a75953b60ae2036d20d22828bcf
JuanCM <juancampuzano2356@gmail.com>
2024-05-11 11:30:26 -0500

    Sección final - Clase 15

--- date/funciones-fechas/funciones-fechas.php
+++ date/funciones-fechas/funciones-fechas.php
@@ -19,4 +19,20 @@ $dateNow = date("Y-m-d H:i:s");
 // echo date("Y-m-d H:i:s", $unix_time); 
 
 // Fechas inmutables
-$date_inmutable = new DateTimeImmutable(); 
+// $date_inmutable = new DateTimeImmutable(); 
+
+// Diferencia de tiempo 
+// $today = new DateTime();
+// $mrmichi_birth = new DateTime("2020-03-25"); 
+// $interval = $mrmichi_birth->diff($today); 
+
+// echo $interval->format("%y años con %d días"); 
+
+// Crear desde un formato dado
+// $date = DateTime::createFromFormat("l j F Y", "Sunday 17 April 2022");
+// echo $date->format("Y-m-d H:i:s"); 
+
+//Modificar una fecha
+$date = new DateTime();
+$date->modify("+1 day +2 months"); 
+echo $date->format("Y-m-d");
---
+++ date/retax-functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+require('date.php'); 
+
+// echo add_time("2022-04-23 12:45:34", "1 día + 2 horas"); 
+// echo translate_time("1 día", false); 
+// echo get_day_of_the_week(4); 
+// echo get_time_from_timestamp("18:32:45"); 
+// echo get_short_date_from_timestamp("2022-04-25"); 
+// echo get_date_from_timestamp("2022-04-25"); 
